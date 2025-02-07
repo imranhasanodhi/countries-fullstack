cdb7bf86755cbafa82761f6fbfc3f2d9bb8f852d
Martin Holland <martin.holland@free.fi>
2025-02-07 15:03:49 +0200

    end of lesson

---
+++ frontend/src/components/Navigation.tsx
@@ -0,0 +1,29 @@
+import { AppBar, Button, Toolbar } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export const Navigation = () => {
+  const { user, signOut } = useAuth();
+
+  return (
+    <AppBar position="static" sx={{ mb: 3 }}>
+      <Toolbar>
+        <Button color="inherit" component={RouterLink} to="/">
+          Home
+        </Button>
+        <Button color="inherit" component={RouterLink} to="/test">
+          Test
+        </Button>
+        {user ? (
+          <Button color="inherit" onClick={signOut}>
+            Logout ({user.email})
+          </Button>
+        ) : (
+          <Button color="inherit" component={RouterLink} to="/login">
+            Login
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
