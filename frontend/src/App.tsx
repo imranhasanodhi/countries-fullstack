1201c3f56fec6677b6dcf8d22e4e2c1595148b24
Martin Holland <martin.holland@free.fi>
2025-02-14 13:54:11 +0200

    Authentication completed and protected data tables and data entry added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Box } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthRedirect } from "./components/Auth/AuthRedirect";
 import { Login } from "./components/Auth/Login";
 import { ProtectedRoute } from "./components/Auth/ProtectedRoute";
 import { Navigation } from "./components/Navigation";
+import ProtectedTestData from "./components/ProtectedTestData";
 import { TestData } from "./components/TestData";
 import { AuthProvider } from "./context/AuthContext";
 
@@ -14,16 +16,25 @@ function App() {
           <Navigation />
           <Box sx={{ p: 3 }}>
             <Routes>
-              <Route path="/login" element={<Login />} />
+              <Route
+                path="/login"
+                element={
+                  <>
+                    <AuthRedirect />
+                    <Login />
+                  </>
+                }
+              />
               <Route path="/test" element={<TestData />} />
               <Route
                 path="/protected"
                 element={
                   <ProtectedRoute>
-                    <></>
+                    <ProtectedTestData />
                   </ProtectedRoute>
                 }
               />
+              <Route path="/" element={<div>Home is here</div>} />
               {/* Other routes... */}
             </Routes>
           </Box>
